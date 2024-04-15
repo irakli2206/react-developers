b6f0d432c26a08a5e39d7103de891dd05101023b
irakli2206 <iraklibego1@gmail.com>
2024-04-15 12:14:05 +0400

    improvments on country select

--- app/dashboard/general/page.tsx
+++ app/dashboard/general/page.tsx
@@ -7,36 +7,86 @@ import { Button } from '@/components/ui/button'
 import { useFormState, useFormStatus } from 'react-dom'
 import { updateGeneralInfo } from './action'
 import { createClient } from '@/utils/supabase/client'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { Check, CheckIcon, ChevronDown } from 'lucide-react'
+import { cn } from '@/lib/utils'
+import {
+    Calculator,
+    Calendar,
+    CreditCard,
+    Settings,
+    Smile,
+    User,
+} from "lucide-react"
+
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+    CommandShortcut,
+} from "@/components/ui/command"
 
 const supabase = createClient()
 
 const General = () => {
 
-    const [profile, setProfile] = useState<any>()
+    const [profile, setProfile] = useState<any>({})
+
+    const [countryOptions, setCountryOptions] = useState<string[]>([])
+
+    const getProfile = async () => {
+        const { data: userData, error: userError } = await supabase.auth.getUser()
+        const { data: profileData }: any = await supabase.from('profiles').select('*').eq('id', userData.user?.id)
+
+        const formattedProfileData = { ...profileData[0] }
+        if (!formattedProfileData.skills) formattedProfileData.skills = []
+
+        setProfile(formattedProfileData)
+    }
+
+    const getCountryOptions = async () => {
+        try {
+            const countries = await fetch("https://restcountries.com/v3.1/all?fields=name")
+            const countriesData = await countries.json()
+            const countryNames = countriesData.map((c: any) => c.name.common).sort((a: any, b: any) => a - b)
+            setCountryOptions(countryNames)
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
     useEffect(() => {
-        const getProfile = async () => {
-            const { data: userData, error: userError } = await supabase.auth.getUser()
-            const { data: profileData }: any = await supabase.from('profiles').select('*').eq('id', userData.user?.id)
-
-            console.log(userData)
-            setProfile(profileData[0])
-        }
         getProfile()
+        getCountryOptions()
     }, [])
+
+    console.log(countryOptions)
+
 
 
     const handleFieldChange = (value: string, fieldName: keyof typeof profile) => {
-        setProfile((prevState: any) => ({
+        const isFieldArray = Array.isArray(profile[fieldName])
+        if (isFieldArray) setProfile((prevState: any) => ({
+            ...prevState,
+            [fieldName]: [...prevState[fieldName], value]
+        }))
+        else setProfile((prevState: any) => ({
             ...prevState,
             [fieldName]: value
         }))
     }
 
-    const handleSave = async() => {
-        const {data, error} = await supabase.from('profiles').update(profile).eq('id', profile.id)
-        console.log(error)
+    const handleSave = async () => {
+        const { data, error, status } = await supabase.from('profiles').update(profile).eq('id', profile.id)
+        console.log(status)
     }
+
+    console.log(profile)
 
     return (
         <div className='w-full'>
@@ -55,8 +105,8 @@ const General = () => {
                             <dd className="  text-zinc-500 ">Your first and last name</dd>
                         </div>
                         <Input className='flex-1 h-9 drop-shadow-sm' type="text" placeholder="John Doe"
-                        value={profile.name}
-                        onChange={(e) => handleFieldChange(e.target.value, 'name')}
+                            value={profile.name}
+                            onChange={(e) => handleFieldChange(e.target.value, 'name')}
                         />
                     </div>
                     <div className="px-4 py-5 flex items-center sm:gap-4 sm:px-0">
@@ -65,7 +115,8 @@ const General = () => {
                             <dd className="  text-zinc-500 ">Summary of your occupation/profession</dd>
                         </div>
                         <Input className='flex-1 h-9 drop-shadow-sm' type="text" placeholder="Senior React Developer"
-                        // defaultValue={profileData[0].title}
+                            value={profile.title}
+                            onChange={(e) => handleFieldChange(e.target.value, 'title')}
                         />
                     </div>
                     <div className="px-4 py-5 flex items-center sm:gap-4 sm:px-0">
@@ -73,7 +124,42 @@ const General = () => {
                             <dt className="  font-medium leading-6 text-gray-900">Country</dt>
                             <dd className="  text-zinc-500 ">Country where you are currently located</dd>
                         </div>
-                        <Input className='flex-1 h-9 drop-shadow-sm' type="text" placeholder="John Doe" />
+                        <Select onValueChange={(e) => handleFieldChange(e, "country")}>
+                            <SelectTrigger className='flex-1 h-9 drop-shadow-sm' >
+                                <SelectValue placeholder={profile.country ? profile.country : "Select country"} />
+                            </SelectTrigger>
+                            <SelectContent >
+                                <Command  >
+                                    <CommandInput className='h-9' placeholder="Type a command or search..." />
+                                    <CommandList>
+                                        <CommandEmpty  >No results found.</CommandEmpty>
+                                        <CommandGroup  >
+                                            {countryOptions.map(c => {
+                                                const isSelected = profile.country === c
+                                                return (
+                                                    <CommandItem
+                                                        value={c}
+                                                        onSelect={(e) => handleFieldChange(e, "country")}
+                                                        className='justify-between'
+                                                    >
+                                                        
+                                                        <span>{c}</span>
+                                                        {isSelected && <Check className="mr-2 h-4 w-4" />}
+                                                    </CommandItem>
+                                                )
+                                            })}
+
+                                        </CommandGroup>
+
+                                    </CommandList>
+                                </Command>
+
+                            </SelectContent>
+                        </Select>
+                        {/* <Input className='flex-1 h-9 drop-shadow-sm' type="text" placeholder="John Doe"
+                            value={profile.country}
+                            onChange={(e) => handleFieldChange(e.target.value, 'country')}
+                        /> */}
                     </div>
 
                     <div className="px-4 py-5 flex items-center sm:gap-4 sm:px-0">
@@ -81,7 +167,58 @@ const General = () => {
                             <dt className="  font-medium leading-6 text-gray-900">Skills</dt>
                             <dd className="  text-zinc-500 ">Your tech stack</dd>
                         </div>
-                        <Input className='flex-1 h-9 drop-shadow-sm' type="text" placeholder="John Doe" />
+                        <Popover   >
+                            <PopoverTrigger asChild>
+                                <button type="button" role="combobox" aria-controls="radix-:R2ejtt7qcq:" aria-expanded="false" aria-autocomplete="none" dir="ltr" data-state="closed" className="flex w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&amp;>span]:line-clamp-1 flex-1 h-9 drop-shadow-sm"><span className='pointer-events-none'>Mayotte</span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-down h-4 w-4 opacity-50" aria-hidden="true"><path d="m6 9 6 6 6-6"></path></svg></button>
+                                {/* <Button
+                                    variant="outline"
+                                    role="combobox"
+                                    aria-expanded={open}
+                                    className="w-[200px] justify-between"
+                                >
+                                    {"Select framework..."}
+                                    <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                                </Button> */}
+                            </PopoverTrigger>
+                            <PopoverContent className="w-[200px] p-0">
+                                <Command>
+                                    <CommandInput placeholder="Search framework..." />
+                                    <CommandEmpty>No framework found.</CommandEmpty>
+                                    <CommandGroup>
+                                        {countryOptions && countryOptions.map((framework: string) => (
+                                            <CommandItem
+                                                key={framework}
+                                                value={framework}
+                                            // onSelect={(currentValue) => {
+                                            //     setValue(currentValue === value ? "" : currentValue)
+                                            //     setOpen(false)
+                                            // }}
+                                            >
+                                                <Check
+                                                    className={cn(
+                                                        "mr-2 h-4 w-4",
+                                                        // value === framework.value ? "opacity-100" : "opacity-0"
+                                                    )}
+                                                />
+                                                {framework}
+                                            </CommandItem>
+                                        ))}
+                                    </CommandGroup>
+                                </Command>
+                            </PopoverContent>
+                        </Popover>
+                        {/* <Select value={profile.country} onValueChange={(e) => handleFieldChange(e, "skills")}>
+                            <SelectTrigger className='flex-1 h-9 drop-shadow-sm' >
+                                <SelectValue placeholder="Select skills" />
+                            </SelectTrigger>
+                            <SelectContent >
+                                {countryOptions.map(c => {
+                                    return <SelectItem value={c} >
+                                        {c}
+                                    </SelectItem>
+                                })}
+                            </SelectContent>
+                        </Select> */}
                     </div>
                     <div className="px-4 py-5 flex items-center sm:gap-4 sm:px-0">
                         <div className='flex flex-col gap-1 flex-1 text-sm'>
@@ -95,7 +232,10 @@ const General = () => {
                             <dt className="  font-medium leading-6 text-gray-900">Bio</dt>
                             <dd className="  text-zinc-500 ">A few words about you</dd>
                         </div>
-                        <Textarea className='flex-1 drop-shadow-sm' placeholder="Tell us about yourself" />
+                        <Textarea className='flex-1 drop-shadow-sm' placeholder="Tell us about yourself"
+                            value={profile.bio}
+                            onChange={(e) => handleFieldChange(e.target.value, 'bio')}
+                        />
                     </div>
                 </dl>
             </div>
