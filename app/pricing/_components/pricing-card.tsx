a391fa4bc77de8889c631a9f8ead226a95cedc48
irakli2206 <iraklibego1@gmail.com>
2024-04-17 17:54:10 +0400

    .

--- app/pricing/_components/pricing-card.tsx
+++ app/pricing/_components/pricing-card.tsx
@@ -1,19 +1,41 @@
-import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
+import { CheckCircle2 } from 'lucide-react'
 import React from 'react'
 
-const PricingCard = () => {
+type Props = {
+  title: "Developer" | "Employer"
+  subtitle: string
+  cost: string
+  features: string[]
+  button: React.ReactNode
+}
+
+const PricingCard = ({ title, subtitle, cost, features, button }: Props) => {
   return (
-    <Card>
-        <CardHeader>
-            <h1 className='text-3xl font-bold'>Developer</h1>
-            <p className='text-zinc-400'>Add your profile to ReactDevelopers</p>
-        </CardHeader>
-        <CardContent>
-            <h1 className='text-2xl font-semibold'>Free</h1>
-            <div className="flex flex-col gap-2">
-                
-            </div>
-        </CardContent>
+    <Card className='flex-1 px-6 py-4 flex flex-col h-[470px] max-w-[400px]'>
+      <CardHeader>
+        <h1 className='text-3xl font-semibold'>{title}</h1>
+        <p className='text-muted-foreground'>{subtitle}</p>
+      </CardHeader>
+      <CardContent>
+        <h1 className='text-2xl font-medium mb-4'>{cost}</h1>
+        <div className="flex flex-col gap-2">
+          {features.map(feature => {
+            return (
+              <p className='flex gap-2 text-sm text-muted-foreground items-center'>
+                <CheckCircle2 className='text-green-400 fill-green-100' width={18} />
+                {feature}
+              </p>
+            )
+          })}
+
+        </div>
+      </CardContent>
+      <CardFooter className='flex flex-col h-full justify-end'>
+
+        {button}
+      </CardFooter>
     </Card>
   )
 }
--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,14 +1,48 @@
 import React from 'react'
+import PricingCard from './_components/pricing-card'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 const Pricing = () => {
   return (
-    <div className='py-32'>
-        <div className="flex flex-col">
-            <header className='flex flex-col gap-4'>
-                <h1 className=''>We keep it simple</h1>
-            </header>
-        </div>
-    </div>
+    <div className='py-36'>
+      <div className="flex flex-col gap-12">
+        <header className='flex flex-col gap-4 justify-center items-center text-center'>
+          <h1 className='text-5xl font-semibold'>We keep it simple</h1>
+          <p className='text-muted-foreground text-lg'>No 5 different confusing subscription plans, our terms are straightforward</p>
+        </header>
+
+        <main className="flex gap-4 justify-center">
+          <PricingCard
+            title='Developer'
+            subtitle='Add your profile to ReactDevelopers'
+            cost='Free'
+            features={[
+              "Join our database",
+              "Get contacted by employers",
+              "Be discovered by hiring managers",
+              "Free cookie delivery to your door",
+              "Ok last one is a lie"
+            ]}
+            button={<Button variant='outline' asChild className='rounded-full w-full'>
+              <Link href='signup'>Create Account</Link></Button>}
+          />
+
+          <PricingCard
+            title='Employer'
+            subtitle='Start hiring ReactJS talent'
+            cost='$137.00'
+            features={[
+              "Full access to developer info",
+              "Ability to use search filters",
+              "Greater prioritization of your feature requests",
+            ]}
+            button={<Button variant='default' asChild className='rounded-full w-full'>
+              <Link href='signup'>Subscribe</Link></Button>}
+          />
+        </main>
+      </div>
+    </div >
   )
 }
 
