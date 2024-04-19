c645c0051cef41891bb8ecab90dc97d7314a41a7
irakli2206 <iraklibego1@gmail.com>
2024-04-19 21:48:57 +0400

    deleted checkout

--- app/api/checkout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export function GET(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  return Response.json({ message: 'Hellos from Next.js!' })
-}
