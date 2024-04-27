d50191568e4d033a5dd9608777ba1ecde24538ac
irakli2206 <iraklibego1@gmail.com>
2024-04-27 17:30:17 +0400

    functional cta

--- components/sections/home/CTA.tsx
+++ components/sections/home/CTA.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const CTA = () => {
   return (
-    <div className='max-w-7xl container py-32 flex flex-col gap-4'>
+    <div className='max-w-7xl container pt-32 flex flex-col gap-4'>
       <div className=" shadow rounded-3xl w-full px-12 py-24  relative overflow-hidden">
         <Image
           src='/cta-bg.png'
@@ -19,8 +20,12 @@ const CTA = () => {
             <p className='text-xl font-light text-gray-200'>Become one of the first in the reverse job board revolution, or book a call with the founder</p>
 
             <div className="flex gap-2 mt-8">
-              <Button variant='secondary' className=' shadow-[0px_0px_8px_2px_white] w-[200px]' >Sign up</Button>
-              <Button variant='secondary' className=' bg-transparent border border-gray-300 text-white w-[200px] hover:bg-gray-300/20' >Book call</Button>
+              <Button variant='secondary' className=' shadow-[0px_0px_8px_2px_white] w-[200px]' asChild>
+                <Link href='/signup'>Sign up</Link>
+              </Button>
+              <Button variant='secondary' className=' bg-transparent border border-gray-300 text-white w-[200px] hover:bg-gray-300/20' asChild>
+              <Link href='https://calendly.com/iraklibego1/45min' target='_blank'>Book call</Link>
+              </Button>
 
             </div>
           </div>
