a9999ea25d690ffd8a54cc47455c7e0d0791ba86
irakli2206 <iraklibego1@gmail.com>
2024-04-29 12:51:10 +0400

    back to retard mode

--- env.ts
+++ env.ts
@@ -1,2 +1,3 @@
 
-export const BASE_URL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : process.env.NEXT_PUBLIC_URL
+// export const BASE_URL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : process.env.NEXT_PUBLIC_URL
+export const BASE_URL = process.env.NEXT_PUBLIC_URL
